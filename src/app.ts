09b5cd84766f69b60ba81d3b27e2e585330fafa6
Damian Gasiorowski <damian.gasiorowski@god-se.com>
2022-01-30 14:03:58 +0100

    project lists class

--- src/app.ts
+++ src/app.ts
@@ -10,20 +10,20 @@ interface Validatable {
 
 function validate(validatableInput: Validatable): boolean {
     let isValid = true;
-    if(validatableInput.required) {
-        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
     }
 
-    if(validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+    if (validatableInput.minLength != null && typeof validatableInput.value === 'string') {
         isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
     }
-    if(validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+    if (validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
         isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
     }
-    if(validatableInput.min != null && typeof validatableInput.value === 'number') {
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
         isValid = isValid && validatableInput.value >= validatableInput.min;
     }
-    if(validatableInput.max != null && typeof validatableInput.value === 'number') {
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
         isValid = isValid && validatableInput.value <= validatableInput.max;
     }
     return isValid;
@@ -40,6 +40,35 @@ function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
     };
 }
 
+// ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${type}-projects`;
+
+        this.attach();
+        this.rednerContent();
+    }
+
+    private rednerContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+
+}
 
 // ProjectInput CLass
 class ProjectInput {
@@ -74,22 +103,22 @@ class ProjectInput {
         const titleValidatable: Validatable = {
             value: enteredTitle,
             required: true
-        }
+        };
 
         const descriptionValidatable: Validatable = {
             value: enteredDescription,
             required: true,
             minLength: 5
-        }
+        };
 
         const peopleValidatable: Validatable = {
             value: enteredPeople,
             required: true,
             min: 1,
             max: 5
-        }
+        };
 
-        if(
+        if (
             !validate(titleValidatable) ||
             !validate(descriptionValidatable) ||
             !validate(peopleValidatable)
@@ -106,11 +135,12 @@ class ProjectInput {
         this.descriptionInputElement.value = '';
         this.peopleInputElement.value = '';
     }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         const userInput = this.gatherUserInput();
-        if(Array.isArray(userInput)) {
+        if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
             console.log(title, desc, people);
             this.clearInputs();
@@ -127,3 +157,6 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
